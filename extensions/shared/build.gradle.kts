e7fa6d2e857311a59ada816b99ceb7f7d5e4d057
Luis Cupul 04 <256816875+LuisCupul04@users.noreply.github.com>
2026-04-23 09:27:57 -0600

    Disable protobuf configuration
    Comment out protobuf configuration in build.gradle.kts

--- extensions/shared/build.gradle.kts
+++ extensions/shared/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("com.android.library")
+    //id("com.android.library")
     //id("com.android.application")
     //alias(libs.plugins.kotlin.android)
     alias(libs.plugins.protobuf)
@@ -60,17 +60,17 @@ dependencies {
     compileOnly(project(":extensions:shared:stub"))
 }
 
-protobuf {
-    protoc {
-        artifact = libs.protobuf.protoc.get().toString()
-    }
-    generateProtoTasks {
-        all().forEach { task ->
-            task.builtins {
-                create("java") {
-                    option("lite")
-                }
-            }
-        }
-    }
-}
+// protobuf {
+//     protoc {
+//         artifact = libs.protobuf.protoc.get().toString()
+//     }
+//     generateProtoTasks {
+//         all().forEach { task ->
+//             task.builtins {
+//                 create("java") {
+//                     option("lite")
+//                 }
+//             }
+//         }
+//     }
+// }
