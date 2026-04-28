37fc3d90e790448a07befdaab4be5312612a6fe7
Luis Cupul 04 <256816875+LuisCupul04@users.noreply.github.com>
2026-04-28 06:13:22 -0600

    Clean up build.gradle.kts by removing comments
    Removed commented Android plugin line and updated protobuf dependency comment.

--- extensions/shared/build.gradle.kts
+++ extensions/shared/build.gradle.kts
@@ -1,7 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-// No se aplican plugins de Android
-
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
@@ -21,9 +19,7 @@ dependencies {
     implementation(libs.gson)
     implementation(libs.lang3)
     implementation(libs.okhttp3)
-    // implementation(libs.protobuf.javalite)  // comentado por ahora
-
+    // implementation(libs.protobuf.javalite)   // Comenta temporalmente
     implementation("com.github.ynab:J2V8:6.2.1-16kb.2@aar")
-
     compileOnly(project(":extensions:shared:stub"))
 }
